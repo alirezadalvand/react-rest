2208aa283214b10918a2e7b256ff77d3ec353831
alirezadalvand <alireza.d1339@gmail.com>
2021-06-23 11:38:55 -0700

    app js added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>alireza</h1>
+    </>
+  );
+}
+
+export default App;
